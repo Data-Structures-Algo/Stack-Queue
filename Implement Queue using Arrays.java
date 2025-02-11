e653fd01291421361437ac6aa8e8a66c64df5f58
Gourav Kumar <137706422+Gouravkumar08@users.noreply.github.com>
2025-02-11 18:02:29 +0530

    Update Implement Queue using Arrays.java

--- Implement Queue using Arrays.java
+++ Implement Queue using Arrays.java
@@ -62,3 +62,41 @@ public class Queues {
         }
     }
 }
+
+
+// GFG Question
+class MyQueue {
+
+    int front, rear;
+	int arr[] = new int[100005];
+
+    MyQueue()
+	{
+		front=0;
+		rear=0;
+	}
+	
+	//Function to push an element x in a queue.
+	void push(int x)
+	{
+	    // Your code here
+	    if(rear==arr.length){
+	        return ;
+	    }
+	    
+	    arr[rear]=x;
+	    rear++;
+	} 
+
+    //Function to pop an element from queue and return that element.
+	int pop()
+	{
+		// Your code here
+		if(front ==rear){
+		    return -1;
+		}
+		int result = arr[front];
+		front++;
+		return result;
+	} 
+}
